bd74c5718f776b30292390d7b470849a64f1ec1b
Daniel Dörrhöfer <pcre@gmx.de>
2017-01-12 22:08:30 +0100

    Removed trailing whitespace

--- fuzzy-freifunk.R
+++ fuzzy-freifunk.R
@@ -5,8 +5,6 @@ p_load(jsonlite, RecordLinkage)
 
 fugrep = function(string, stringVector){
   distance = levenshteinSim(string, stringVector);
-  #stringVector[distance == max(distance)]
-  #which(distance == max(distance))
   order(distance)
 }
 
@@ -15,16 +13,16 @@ node <- args[6]
 community <- args[7]
 num <- args[8]
 num <- as.numeric(num)
-if (is.na(num)) num <- 1 
+if (is.na(num)) num <- 10
 
-#j <- fromJSON("freifunk-karte-data.json")
 j <- fromJSON("http://api.freifunk.net/data/freifunk-karte-data.json")
 router <- j$allTheRouters
+
 nodesearch <- function(node, coty="all") {
   if (missing(node)) stop()
   as.character(node)
   as.character(coty)
-  
+
   if (is.na(coty) || coty == "all") {
     s <- router
   } else {
@@ -35,10 +33,7 @@ nodesearch <- function(node, coty="all") {
 
 postit <- function(n)
 {
-  #cat(paste(as.character(n[-8]), collapse="  "), "\n")
-  write(paste(as.character(n[-8]), collapse="  "), file="/home/daniel/ircbot/irc.freenode.net/#d4ndo/in")
-  write(paste(as.character(n[-8]), collapse="  "), file="/home/daniel/ircbot/irc.hackint.net/#ffmwu/in")
-
+  cat(paste(as.character(n[-8]), collapse="  "), "\n")
 }
 
 res <- nodesearch(node, community)
