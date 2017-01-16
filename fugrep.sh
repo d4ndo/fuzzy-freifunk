b484954bdd836c3adf114d3505b65210fa31ffde
Daniel Dörrhöfer <pcre@gmx.de>
2017-01-16 16:38:44 +0100

    using curl to pull api file.

--- fugrep.sh
+++ fugrep.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-declare -a namearray=( $(cat freifunk-karte-data.json | jq -r '.allTheRouters[] | .name | tostring' | fzf -m) )
+freifunk="$(curl https://api.freifunk.net/data/freifunk-karte-data.json)"
+
+declare -a namearray=( $(echo "$freifunk" | jq -r '.allTheRouters[] | .name | tostring' | fzf -m) )
 
 for n in ${namearray[@]}
 do
     name="$(tr -d '"' <<< $n)"
-    e="$(cat freifunk-karte-data.json | jq -r ".allTheRouters[] | select(.name == \"$name\") | .name, .community, .status, .clients")"
+    e="$(echo "$freifunk" | jq -r ".allTheRouters[] | select(.name == \"$name\") | .name, .community, .status, .clients")"
     echo -e "$e"
 done
